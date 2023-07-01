864ae7e29fa5bdeb1d509cb8586b90209eca0402
Mateusz Majer <93955642+Vectari@users.noreply.github.com>
2023-07-01 13:05:13 +0200

    Separating Gamepad API and INFO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-import { Gamepad } from "@/GamepadAPI/GamepadAPI";
 import { AxesSVG } from "@/SVG/AxesSVG";
 import { XboxSVG } from "@/SVG/XboxSVG";
 import { PS4SVG } from "@/SVG/PS4SVG";
+import { GamepadInfo } from "@/GamepadAPI/GamepadInfo";
+import { GamepadAPI } from "@/GamepadAPI/GamepadAPI";
 
 export default function Home() {
   return (
     <>
-      <XboxSVG />
-      <PS4SVG />
+      {/* <XboxSVG /> */}
+      {/* <PS4SVG /> */}
       <AxesSVG />
-      <div className="p-24">
-        <Gamepad />
-      </div>
+      <GamepadInfo />
+      <GamepadAPI />
     </>
   );
 }
