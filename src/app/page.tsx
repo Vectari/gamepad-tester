eab85c8c3739e2f223e80a8bbd93a627652a858e
Mateusz Majer <93955642+Vectari@users.noreply.github.com>
2023-07-01 14:48:34 +0200

    Add some working buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
+"use client";
+
 import { AxesSVG } from "@/SVG/AxesSVG";
 import { XboxSVG } from "@/SVG/XboxSVG";
 import { PS4SVG } from "@/SVG/PS4SVG";
 import { GamepadInfo } from "@/GamepadAPI/GamepadInfo";
-import { GamepadAPI } from "@/GamepadAPI/GamepadAPI";
+import { GamepadAPI, atomGamepadName } from "@/GamepadAPI/GamepadAPI";
+import { useAtom } from "jotai/react";
 
 export default function Home() {
+  // const [gamepadName] = useAtom(atomGamepadName);
+
+  // if (`${gamepadName}` === `${"Xbox 360 Controller (XInput STANDARD GAMEPAD)"}`) {
+  //    return <XboxSVG />
+  // } else {
+  //   <PS4SVG />
+  // }
+
   return (
     <>
-      {/* <XboxSVG /> */}
-      {/* <PS4SVG /> */}
+      <XboxSVG />
+      <PS4SVG />
       <AxesSVG />
       <GamepadInfo />
       <GamepadAPI />
