5598c67b3d648eafb0b35cdd6c96081056f7fd57
Mateusz Majer <93955642+Vectari@users.noreply.github.com>
2023-07-03 23:37:32 +0200

    If else XBOX/PS4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,21 +8,53 @@ import { GamepadAPI, atomGamepadName } from "@/GamepadAPI/GamepadAPI";
 import { useAtom } from "jotai/react";
 
 export default function Home() {
-  // const [gamepadName] = useAtom(atomGamepadName);
+  const [gamepadName] = useAtom(atomGamepadName);
 
-  // if (`${gamepadName}` === `${"Xbox 360 Controller (XInput STANDARD GAMEPAD)"}`) {
-  //    return <XboxSVG />
-  // } else {
-  //   <PS4SVG />
-  // }
+  if (
+    `${gamepadName}` === `${"Xbox 360 Controller (XInput STANDARD GAMEPAD)"}`
+  ) {
+    return (
+      <>
+        <div className="p-2">
+          <XboxSVG />
+        </div>
+        <AxesSVG />
+        <GamepadInfo />
+        <GamepadAPI />
+      </>
+    );
+  } else if (`${gamepadName}` === "") {
+    return (
+      <>
+        <AxesSVG />
+        <GamepadInfo />
+        <GamepadAPI />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="-mt-20 p-2">
+          <PS4SVG />
+        </div>
+        <AxesSVG />
+        <GamepadInfo />
+        <GamepadAPI />
+      </>
+    );
+  }
 
-  return (
-    <>
-      <XboxSVG />
-      <PS4SVG />
-      <AxesSVG />
-      <GamepadInfo />
-      <GamepadAPI />
-    </>
-  );
+  // return (
+  //   <>
+  //     <div className="p-2">
+  //       <XboxSVG />
+  //     </div>
+  //     <div className="-mt-20 p-2">
+  //       <PS4SVG />
+  //     </div>
+  //     <AxesSVG />
+  //     <GamepadInfo />
+  //     <GamepadAPI />
+  //   </>
+  // );
 }
