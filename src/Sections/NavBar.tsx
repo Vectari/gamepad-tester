4444c7fc04a0305f4dd6e51960704cd97ec5c758
Mateusz Majer <93955642+Vectari@users.noreply.github.com>
2023-07-08 01:16:08 +0200

    Add some test styles

--- src/Sections/NavBar.tsx
+++ src/Sections/NavBar.tsx
@@ -1,14 +1,27 @@
 "use client";
 
+import { styled } from "styled-components";
+
+const Title = styled.h1`
+  color: red;
+`;
+
+const Button = styled.button`
+  background-color: yellow;
+  padding: 4px;
+  border-radius: 5px;
+  font-size: 20px;
+`;
+
 export default function NavBar() {
   return (
     <div>
-      <h1>Gamepad Tester</h1>
-      <button>Button 1</button>
-      <button>Button 2</button>
-      <button>Button 3</button>
-      <button>Button 4</button>
-      <button>Button 5</button>
+      <Title>Gamepad Tester</Title>
+      <Button>Button 1</Button>
+      <Button>Button 2</Button>
+      <Button>Button 3</Button>
+      <Button>Button 4</Button>
+      <Button>Button 5</Button>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 "use client";
 
+import { styled } from "styled-components";
 import GamepadTester from "@/Sections/GamepadTester";
 import NavBar from "@/Sections/NavBar";
+
+const NavBarWrapper = styled.nav`
+background-color: pink;
+padding: 5px;
+border-radius: 0 0 5px 5px;
+`;
 
 export default function Home() {
   return (
     <>
-      <NavBar />
+      <NavBarWrapper>
+        <NavBar />
+      </NavBarWrapper>
       <GamepadTester />
     </>
   );
