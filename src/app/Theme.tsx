af1b58babb15d86e2fba2f2612bc87f6bb3c6130
Mateusz Majer <93955642+Vectari@users.noreply.github.com>
2023-09-11 23:15:54 +0200

    style: add basic color palette

--- src/app/Theme.tsx
+++ src/app/Theme.tsx
@@ -1,6 +1,13 @@
 export const Theme = {
-  colors: {
-    pink: "pink",
+  BasicColors: {
+    black: "#0B0C10",
+    bunker: "#1F2833",
+    lightgrey: "#C5C6C7",
+    turquoise: "#66FCF1",
+    darkturquoise: "#45A29E",
+  },
+  SVGColors: {
+    black: "#000",
     white: "#FFF",
   },
 };
