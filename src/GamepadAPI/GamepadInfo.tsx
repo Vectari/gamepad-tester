b28d7910a4880cdcf18295ba09fe1340e55c188c
Mateusz Majer <93955642+Vectari@users.noreply.github.com>
2023-09-15 23:45:11 +0200

    style: style  in gamepadInfo

--- src/GamepadAPI/GamepadInfo.tsx
+++ src/GamepadAPI/GamepadInfo.tsx
@@ -63,93 +63,33 @@ export function GamepadInfo() {
   const [buttons] = useAtom(atomButtons);
   const [axes] = useAtom(atomAxes);
 
-  const GamepadInfoWrapper = styled.div`
-    background-color: #f1e7e7;
-    border-radius: 5px 5px 0 0;
-    margin: 2px;
-    padding: 10px;
-    width: 100vw;
-    position: relative;
-
-    @media (min-width: 425px) {
-      width: 400px;
-      margin: 2px auto;
-    }
-    @media (min-width: 768px) {
-      width: 600px;
-    }
-  `;
-
-  const StyledNotConnectedInfo = styled.h1``;
-
-  const StyledLoader = styled.div`
-    border: 16px solid ${Theme.BasicColors.white};
-    border-top: 16px solid ${Theme.BasicColors.darkturquoise};
-    border-radius: 50%;
-    width: 120px;
-    height: 120px;
-    animation: spin 2s linear infinite;
-    @keyframes spin {
-      0% {
-        transform: rotate(0deg);
-      }
-      100% {
-        transform: rotate(360deg);
-      }
-    }
-  `;
-
-  const StyledAxesInfo = styled.div`
-    display: flex;
-    justify-content: space-between;
-  `;
-
-  const StyledButtons = styled.div`
-    background-color: silver;
-    padding: 5px;
-    width: 68px;
-    display: inline-block;
-    margin: 5px;
-    border-radius: 5px;
-  `;
-
-  const StyledSVG = styled.svg`
-    padding-left: 5px;
-    height: 25px;
-    position: absolute;
-    border-radius: 5px;
-  `;
-
   let buttonsNumber = [];
 
   for (let i = 0; i < buttons; i++) {
-    let bValue = navigator?.getGamepads()[0]?.buttons[i].value;
-    buttonsNumber.push(
-      <StyledButtons>
-        B {i}:{" "}
-        {
-          <StyledSVG>
-            <svg width="10px" height={bValue! * 20}>
-              <rect
-                width="7px"
-                height="20px"
-                fill={bValue == 1 ? "green" : "red"}
-              ></rect>
-            </svg>
-          </StyledSVG>
-        }
-      </StyledButtons>
-    );
+    let buttonsValue = navigator?.getGamepads()[0]?.buttons[i].value;
+
+    // `rgba(0,0,0,${lt})` : "rgb(255, 255, 255)"
+
+    const StyledButtons = styled.div`
+      background-color: ${buttonsValue! > 0 ? `rgba(0,0,0,${buttonsValue})` : `${Theme.BasicColors.white}`};
+      color: ${Theme.BasicColors.darkturquoise};
+      padding: 5px;
+      margin: 5px;
+      border-radius: 10px;
+      width: 45px;
+    `;
+    buttonsNumber.push(<StyledButtons>B {i}</StyledButtons>);
   }
 
   let axesNumber = [];
 
   for (let i = 0; i < axes; i++) {
-    let aValue = navigator?.getGamepads()[0]?.axes;
+    let axesValue = navigator?.getGamepads()[0]?.axes;
     axesNumber.push(
       <StyledAxesInfo>
         <div>
-          Axis {i}: {Math.abs(aValue![i]).toFixed(3).toString().substring(0, 5)}
+          Axis {i}:{" "}
+          {Math.abs(axesValue![i]).toFixed(3).toString().substring(0, 5)}
         </div>
       </StyledAxesInfo>
     );
@@ -176,9 +116,50 @@ export function GamepadInfo() {
           {axesNumber}
           {buttonsNumber}
           <div>{<AxesSVG />}</div>
+          <GamepadTester />
         </GamepadInfoWrapper>
         <GamepadAPI />
       </>
     );
   }
 }
+
+const GamepadInfoWrapper = styled.div`
+  background-color: #f1e7e7;
+  border-radius: 5px 5px 0 0;
+  padding: 20px 40px;
+  position: relative;
+  /* 
+    @media (min-width: 425px) {
+      width: 400px;
+      margin: 2px auto;
+    }
+    @media (min-width: 768px) {
+      width: 600px;
+    } */
+`;
+
+const StyledNotConnectedInfo = styled.h1``;
+
+const StyledLoader = styled.div`
+  border: 16px solid ${Theme.BasicColors.white};
+  border-top: 16px solid ${Theme.BasicColors.darkturquoise};
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+  animation: spin 2s linear infinite;
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const StyledAxesInfo = styled.div`
+  display: inline-block;
+  padding-right: 15px;
+  width: 100px;
+`;
