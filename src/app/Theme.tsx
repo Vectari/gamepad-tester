af768e0faef9442a78f7d58645c27bd779a335c4
Mateusz Majer <93955642+Vectari@users.noreply.github.com>
2023-09-18 23:17:06 +0200

    style: more Theme colors

--- src/app/Theme.tsx
+++ src/app/Theme.tsx
@@ -5,6 +5,8 @@ export const Theme = {
     title: "#D96C06",
     navbar: "#2D2D2A",
     panelBackground: "#dfdbdccc",
+    error: "#e01e37",
+    ok: "#38b000",
   },
   SVGColors: {
     black: "#000",
